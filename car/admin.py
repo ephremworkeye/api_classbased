b8f4465a55e739885dfe89642ea93cf69ec23f49
Ephrem Wube <ephremworkeye@gmail.com>
2021-07-31 21:36:39 -0700

    models for Manufactorer and Car added

---
+++ car/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+# from .models import Manufacturer, Car
+
+# Register your models here.
---
+++ car/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    make = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    car_model = models.CharField(max_length=100)
+    car_year = models.IntegerField()
+
+    def __str__(self):
+        return self.make
+    
+
+
